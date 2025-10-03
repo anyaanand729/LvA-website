d9bc50cd8e1e9ea34d40ebfbb2879680e58b8a3a
anyaanand729 <126aanand@frhsd.com>
2025-10-03 11:01:30 -0400

    thirdPush

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+let array1 = ["Image 1 of 4", "Image 2 of 4", "Image 3 of 4", "Image 4 of 4"];
+let array = ["LVA1.png", "LVA2.png", "reCAPTCHA.png", "Duolingo.png"];
+let x = 0;
 function plusImage(){
-    document.getElementById("myImage").innerHTML = "100%";
+    if (x===3){
+        x=0;
+    }
+    else{
+        x++;
+    }
+    displayImage()
+}
+function minusImage(){
+    if(x===0){
+        x=3;
+    }
+    else{
+        x--;
+    }
+    displayImage()
+}
+function displayImage(){
+    document.getElementById("images").src=array[x];
+    document.getElementById("cap").innerHTML=array1[x];
+
+}
+let j=0;
+function changeTime(){
+    if(j===0){
+        document.getElementById("entire").style.color="white";
+        document.getElementById("entire").style.backgroundColor="black";
+        document.getElementById("changeDay").innerHTML="☀️";
+        document.getElementById("changeDay").style.backgroundColor="#e1c39e";
+        j=1;
+    }
+    else{
+        document.getElementById("entire").style.color="black";
+        document.getElementById("entire").style.backgroundColor="#e1c39e";
+        document.getElementById("changeDay").innerHTML="🌙";
+        document.getElementById("changeDay").style.backgroundColor="black";
+        j=0;
+    }
+
 }
